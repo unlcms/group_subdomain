eecb2b896247fedd62fbd703251a8661c346f34e
Tyler Lemburg <trlemburg@gmail.com>
2016-12-09 11:31:36 -0600

    Add OgSubdomainPathProcessor
    This path processor looks if there is a subdomain, and sticks that subdomain
    onto the front of the path. This is done because with a single site
    instance, we want different sites to be able to have pages with the same
    url (e.g. "/about-us"). This processor fires BEFORE the route processor
    which takes the resulting URL and looks for a path alias.
    
    Thus, future commits and work will need to make the URL pattern
    /subdomain/rest-of-url common for all pages.

--- src/PathProcessor/OgSubdomainPathProcessor.php
+++ src/PathProcessor/OgSubdomainPathProcessor.php
@@ -5,14 +5,17 @@ namespace Drupal\og_subdomain\PathProcessor;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\PathProcessor\InboundPathProcessorInterface;
 use Drupal\Core\PathProcessor\OutboundPathProcessorInterface;
-use Drupal\Core\Render\BubbleableMetadata;
 use Drupal\Core\Site\Settings;
+use Drupal\og\Og;
+use Drupal\Core\Entity\Entity;
+use Drupal\node\Entity\Node;
+
 use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Processes the inbound path using path alias lookups.
  */
-class OgSubdomainPathProcessor implements OutboundPathProcessorInterface, InboundPathProcessorInterface {
+class OgSubdomainPathProcessor implements InboundPathProcessorInterface {
 
   /**
    * To list all entity types.
@@ -26,11 +29,13 @@ class OgSubdomainPathProcessor implements OutboundPathProcessorInterface, Inboun
    */
   protected $settings;
 
+  protected $query;
+
   /**
    * Constructs a PathProcessorAlias object.
    *
-   * @param \Drupal\Core\Path\AliasManagerInterface $alias_manager
-   *   An alias manager for looking up the system path.
+   * @param \Drupal\Core\Path\AliasManagerInterface $settings
+   *   Settings
    *
    * @param \Drupal\Core\Entity\EntityStorageInterface $entity_storage
    *    Entity storage for looking up the subdomain records on particular entities.
@@ -41,39 +46,20 @@ class OgSubdomainPathProcessor implements OutboundPathProcessorInterface, Inboun
   }
 
   /**
-   * If the request includes a subdomain, set the path accordingly.
+   * If the request includes a subdomain, change the path
    */
   public function processInbound($path, Request $request) {
-    $base_host = $this->settings->get('subdomain_base_url');
-    $host = $request->getHost();
-    $subdomain = substr($host, 0, ((strlen($base_host) + 1) * -1));
+      $host = $request->getHttpHost();    
 
-    // If we have a subdomain, find the entity with which it is associated and update path.
-    if ($subdomain) {
-      // Get all entity types
-      $entity_types = $this->entityTypeManager->getDefinitions();
-      // For each content entity type...
-//      foreach ($this->subdomainInfo->selectContentEntities($entity_types) as $type_name => $type) {
-//        // If it has a subdomain handler...
-//        if ($type->hasHandlerClass('subdomain')) {
-//          // Get the handler and pass the path and request to it.
-//          $subdomain_handler = $this->entityTypeManager->getHandler($type->id(), 'subdomain');
-//          $subdomain_handler->processInbound($path, $subdomain);
-//        }
-//      }
-    }
-    return $path;
-  }
+      $subdomain = explode('.', $host)[0];
 
-  /**
-   * {@inheritdoc}
-   */
-  public function processOutbound($path, &$options = array(), Request $request = NULL, BubbleableMetadata $bubbleable_metadata = NULL) {
-    /*if (empty($options['alias'])) {
-      $langcode = isset($options['language']) ? $options['language']->getId() : NULL;
-      $path = $this->aliasManager->getAliasByPath($path, $langcode);
-    }*/
-    return $path;
+      // if there is a subdomain
+      if (!empty($subdomain) && $subdomain != 'www') {
+          // stick the subdomain in front of the path
+          $path = '/' . $subdomain . $path;
+      }
+
+      return $path;
   }
 
 }

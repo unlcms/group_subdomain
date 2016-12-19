181aea46870f191092e0afeb9b0acec21d540a9a
Tyler Lemburg <trlemburg@gmail.com>
2016-12-19 10:49:13 -0600

    Fix and make more robust

--- src/PathProcessor/OgSubdomainPathProcessor.php
+++ src/PathProcessor/OgSubdomainPathProcessor.php
@@ -16,12 +16,25 @@ use Symfony\Component\HttpFoundation\Request;
 class OgSubdomainPathProcessor implements InboundPathProcessorInterface, OutboundPathProcessorInterface {
 
   protected $entityTypeManager;
-
   protected $settings;
+
+  protected $known_subdomains;
 
   public function __construct(Settings $settings, EntityTypeManagerInterface $entity_type_manager) {
     $this->settings = $settings;
     $this->entityTypeManager = $entity_type_manager;
+
+    // get the known subdomains
+    $query = \Drupal::database()->select('node_field_data', 'nfd');
+    $query->addField('nfd', 'og_subdomain');
+    $query->distinct();
+    $rows = $query->execute()->fetchAll();
+    $this->known_subdomains = array();
+    foreach ($rows as $row) {
+        if (!empty($row->og_subdomain)) {
+            $this->known_subdomains[] = $row->og_subdomain;
+        }
+    }
   }
 
   private function isPathSubbable($path) {
@@ -38,7 +51,10 @@ class OgSubdomainPathProcessor implements InboundPathProcessorInterface, Outboun
           '/themes',
           '/quickedit',
           '/contextual',
-          '/history'
+          '/history',
+          '/core',
+          '/toolbar',
+          '/entity_reference_autocomplete'
       );
 
       foreach($invalid_starting_paths as $invalid_path) {
@@ -60,8 +76,10 @@ class OgSubdomainPathProcessor implements InboundPathProcessorInterface, Outboun
 
           // if there is a subdomain
           if (!empty($subdomain) && $subdomain != 'www') {
-              // stick the subdomain in front of the path
-              $path = '/' . $subdomain . $path;
+              if (in_array($subdomain, $this->known_subdomains)) {
+                  // stick the subdomain in front of the path
+                  $path = '/' . $subdomain . $path;
+              }
           }
       }
       return $path;
@@ -70,11 +88,13 @@ class OgSubdomainPathProcessor implements InboundPathProcessorInterface, Outboun
   public function processOutbound($path, &$options = array(), Request $request = NULL, BubbleableMetadata $bubbleable_metadata = NULL) {
       // strip the subdomain from the front of the path, if it is there.
       if ($this->isPathSubbable($path)) {
-          // remove the subdomain from the front of the path
+          // remove the subdomain from the front of the path, if it matches a known subdomain
           $array = explode('/', $path);
-          array_shift($array);
-          array_shift($array);
-          $path = '/' . implode('/', $array);
+          if (count($array) >= 2 && in_array($array[1], $this->known_subdomains)) {
+              array_shift($array);
+              array_shift($array);
+              $path = '/' . implode('/', $array);
+          }
       }
       return $path;
   }
